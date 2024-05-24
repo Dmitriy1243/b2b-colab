825c7e23aae76ecffe86fa2a180d824c1a22b199
Sergey Kovalev <dipdeepcode@mail.ru>
2024-05-24 22:16:33 +0300

    b2b :: init commit

---
+++ src/test/java/ru/ddc/b2bcolab/B2bColabApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ddc.b2bcolab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2bColabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
